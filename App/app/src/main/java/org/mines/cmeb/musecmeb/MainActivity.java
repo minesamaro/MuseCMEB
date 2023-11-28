500b49b089ebac258b7b94eeffa1aaecc3c727c2
catarinasem <catarina.s.e.moreira@gmail.com>
2023-11-28 01:32:44 +0000

    fragments and bottom navigation bar

--- App/app/src/main/java/org/mines/cmeb/musecmeb/MainActivity.java
+++ App/app/src/main/java/org/mines/cmeb/musecmeb/MainActivity.java
@@ -1,14 +1,47 @@
 package org.mines.cmeb.musecmeb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.mines.cmeb.musecmeb.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.bottomNavigationView.setOnItemSelectedListener(item-> {
+            //switch () {
+                //case R.id.profile4:
+
+                    //break;
+                //case R.id.menu3:
+                    //replaceFragment(new MenuFragment());
+                    //break;
+
+            //}
+            //return true;
+            if(item.getItemId()==R.id.profile4) {
+                replaceFragment(new ProfileFragment());
+            }
+
+            return false;
+        });
+
     }
+    private void replaceFragment (Fragment fragment){
+        FragmentManager fragmentManager= getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+
+
 }
